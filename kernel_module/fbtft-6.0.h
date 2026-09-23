51e8064e98bb15bc56af542a3c974d0875aa756b
Mikaeru <gusiatnikov@bk.ru>
2026-09-24 00:08:01 +0300

    modified:   kernel_module/fbtft-6.0.h

--- kernel_module/fbtft-6.0.h
+++ kernel_module/fbtft-6.0.h
@@ -291,11 +291,11 @@ static int fbtft_driver_probe_pdev(struct platform_device *pdev)           \
 	return fbtft_probe_common(_display, NULL, pdev);                   \
 }                                                                          \
 									   \
-static int fbtft_driver_remove_pdev(struct platform_device *pdev)          \
+static void fbtft_driver_remove_pdev(struct platform_device *pdev)          \
 {                                                                          \
 	struct fb_info *info = platform_get_drvdata(pdev);                 \
 									   \
-	return fbtft_remove_common(&pdev->dev, info);                      \
+	fbtft_remove_common(&pdev->dev, info);                             \
 }                                                                          \
 									   \
 static const struct of_device_id dt_ids[] = {                              \
@@ -322,7 +322,7 @@ static struct platform_driver fbtft_driver_platform_driver = {             \
 		.of_match_table = dt_ids,                                  \
 	},                                                                 \
 	.probe  = fbtft_driver_probe_pdev,                                 \
-	.remove = fbtft_driver_remove_pdev,                                \
+	.remove_new = fbtft_driver_remove_pdev,                                \
 };                                                                         \
 									   \
 static int __init fbtft_driver_module_init(void)                           \
